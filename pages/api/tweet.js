14a8f97d73ffecd6357a30cf4f770e703c26736f
Köksal Kapucuoğlu <31320234+koksalkapucuoglu@users.noreply.github.com>
2021-06-25 01:03:17 +0300

    integrate twitter api

---
+++ pages/api/tweet.js
@@ -0,0 +1,14 @@
+import Twit from '../../lib/twit';
+
+export default function handler(req, res) {
+
+    Twit.get('search/tweets', { from: "ReactExpo", count:10}, function(err, data, response){
+        if(err)
+        {
+            return res.status(400).json(JSON.stringify({message: " Something happened!"}))
+
+        }
+        return res.status(200).json(JSON.stringify(data))
+    })
+
+}
