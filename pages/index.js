39e32beb3e7aa104affd4f4cdfb2c15f36cbbad3
Köksal Kapucuoğlu <31320234+koksalkapucuoglu@users.noreply.github.com>
2021-06-25 01:26:32 +0300

    added twit constraction without swr

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,31 @@ import Layout from "../components/layout";
 import Tweet from "../components/tweet";
 
 function HomePage() {
+  const [data, dataSet] = React.useState([])
+  const [dataLoading, dataLoadingSet] = React.useState(true)
+
+  React.useEffect(()=>{
+
+    async function getData(){
+      const response = await fetch("http://localhost:3000/api/tweet")
+      const  data = await response.json()
+      dataSet(data.statuses)
+      dataLoadingSet(false)
+      console.log(data)
+    }
+  getData()
+}, [])
+
+
   return <Layout>
+
+    {/* varsa dataloading'i göster*/}
+    {dataLoading && <p>Loading</p> }
+
+    {data.map((tweet) => {
+      return <div> {tweet.id} </div>
+        })
+    }
 
     <Tweet name={"Köksal Kapucuoğlu"} slug={"koksall"} datetime={new Date("2021-06-02")} text={`İlk tweet ilk satır
     ikinci satır
